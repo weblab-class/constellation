961e05639bbe74c2e22daf23dec3cb02fd3e2bfb
w-nicole <51725850+w-nicole@users.noreply.github.com>
2021-01-11 17:57:25 -0500

    Removed parent folder for easy access.
    Manual add

---
+++ client/src/components/App.js
@@ -0,0 +1,64 @@
+import React, { Component } from "react";
+import { Router } from "@reach/router";
+import NotFound from "./pages/NotFound.js";
+import Skeleton from "./pages/Skeleton.js";
+
+import "../utilities.css";
+
+import { socket } from "../client-socket.js";
+
+import { get, post } from "../utilities";
+
+/**
+ * Define the "App" component as a class.
+ */
+class App extends Component {
+  // makes props available in this component
+  constructor(props) {
+    super(props);
+    this.state = {
+      userId: undefined,
+    };
+  }
+
+  componentDidMount() {
+    get("/api/whoami").then((user) => {
+      if (user._id) {
+        // they are registed in the database, and currently logged in.
+        this.setState({ userId: user._id });
+      }
+    });
+  }
+
+  handleLogin = (res) => {
+    console.log(`Logged in as ${res.profileObj.name}`);
+    const userToken = res.tokenObj.id_token;
+    post("/api/login", { token: userToken }).then((user) => {
+      this.setState({ userId: user._id });
+      post("/api/initsocket", { socketid: socket.id });
+    });
+  };
+
+  handleLogout = () => {
+    this.setState({ userId: undefined });
+    post("/api/logout");
+  };
+
+  render() {
+    return (
+      <>
+        <Router>
+          <Skeleton
+            path="/"
+            handleLogin={this.handleLogin}
+            handleLogout={this.handleLogout}
+            userId={this.state.userId}
+          />
+          <NotFound default />
+        </Router>
+      </>
+    );
+  }
+}
+
+export default App;
