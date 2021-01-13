3c4f2793173da0d4b6c4f5f31fb980aa3b4ec264
Benjamin Wu <benwu12@mit.edu>
2021-01-12 23:00:11 -0500

    login page

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { Router } from "@reach/router";
 import NotFound from "./pages/NotFound.js";
-import Skeleton from "./pages/Skeleton.js";
+import Login from "./pages/Login.js";
 
 import "../utilities.css";
 
@@ -48,7 +48,7 @@ class App extends Component {
     return (
       <>
         <Router>
-          <Skeleton
+          <Login
             path="/"
             handleLogin={this.handleLogin}
             handleLogout={this.handleLogout}
