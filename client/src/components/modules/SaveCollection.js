51dddef766ff14f345000b5cdeea9204edf0242b
Benjamin Wu <benwu12@mit.edu>
2021-01-16 00:09:56 -0500

    added skeleton code for navbar

---
+++ client/src/components/modules/SaveCollection.js
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+
+import { get } from "../../utilities";
+
+import "./SaveCollection.css";
+ 
+/**
+ * Where most of the user inputs are found - searchbar, displaybar, tagbar, etc. 
+ *
+ * Proptypes
+ * @param {() => ()} resetCanvas clears graph, display bar
+ * 
+ */
+
+class SaveCollection extends Component {
+    constructor(props){
+        super(props);
+    }
+
+    //componentDidMount(){}
+    
+
+    render(){
+        return(
+            <nav>
+                <button>Save Constellation</button>  
+            </nav>
+        )
+    }
+}
+
+export default SaveCollection;
