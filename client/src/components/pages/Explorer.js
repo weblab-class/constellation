612883a82b0b2352b72d7b551be233f4ecab014b
Benjamin Wu <benwu12@mit.edu>
2021-01-16 22:43:27 -0500

    adding props skeleton

--- client/src/components/pages/Explorer.js
+++ client/src/components/pages/Explorer.js
@@ -14,12 +14,12 @@ import { get } from "../../utilities";
  * @param {string} userId id of current logged in user
  */
 
- //DUMMY DATA
- const class6006 = {
-     prereqsToAdd: ["6.001", "6.042", "18.01", "18.02"],
-     coreqsToAdd: [],
-     afterreqsToAdd: ["6.046", "6.854", "6.856"],
- }
+//DUMMY DATA
+const class6006 = {
+    prereqsToAdd: ["6.001", "6.042", "18.01", "18.02"],
+    coreqsToAdd: [],
+    afterreqsToAdd: ["6.046", "6.854", "6.856"],
+}
 
 class Explorer extends Component {
     constructor(props) {
@@ -31,6 +31,8 @@ class Explorer extends Component {
                 coreqsToAdd: [],
                 afterreqsToAdd: [],
             },
+            resetCanvas: false,
+            removeClass: '',
         }
     }
 
@@ -48,9 +50,36 @@ class Explorer extends Component {
         console.log(classData.afterreqsToAdd);
     }
 
+    setCourseObject = (input) => {
+        get("/api/sidebarNode", { subject_id: input }).then((courseArray) => {
+            console.log(courseArray);
+            if (courseArray.length === 0) {
+                this.setState({
+                    courseObject: {
+                        found: false,
+                        searchedText: input
+                    }
+                })
+            }
+            else {
+                const courseObjectFromAPI = courseArray[0];
+                this.setState({
+                    courseObject: {
+                        found: true,
+                        searchedText: input,
+                        prerequisites: courseObjectFromAPI.prerequisites,
+                        subject_id: courseObjectFromAPI.subject_id,
+                        title: courseObjectFromAPI.title,
+                        description: courseObjectFromAPI.description
+                    }
+                });
+            }
+        });
+    }
+
     //returns neighbors for the class and updates state so that network re-renders
     getNeighbors = (inputText) => {
-        return get("/api/graphNode", {subject_id: inputText}).then((graphInfo) => {
+        return get("/api/graphNode", { subject_id: inputText }).then((graphInfo) => {
             const newClassesToAdd = {
                 prereqsToAdd: graphInfo[0].prerequisites,
                 coreqsToAdd: graphInfo[0].corequisites,
@@ -59,36 +88,81 @@ class Explorer extends Component {
             console.log(newClassesToAdd);
             return newClassesToAdd;
         }).catch((err) => {
-          console.log("There was an error retrieving classes.");
-          const newClassesToAdd = {
-            prereqsToAdd: [],
-            coreqsToAdd: [],
-            afterreqsToAdd: [],
-          };
-          return newClassesToAdd;
+            console.log("There was an error retrieving classes.");
+            const newClassesToAdd = {
+                prereqsToAdd: [],
+                coreqsToAdd: [],
+                afterreqsToAdd: [],
+            };
+            return newClassesToAdd;
         });
-      }
+    }
 
-      getDummyNeighbors = (inputText) => {
-          return class6006;
-      }
+    getDummyNeighbors = (inputText) => {
+        return class6006;
+    }
 
+    getCollectionObject = () => {
+
+    }
+
+    handleAddClass = () => {
+
+    }
+
+    handleRemoveClass = () => {
+
+    }
+
+    handleLoadCollection = () => {
+
+    }
+
+    handleCancel = () => {
+
+    }
+
+    handleSaveCollection = () => {
+
+    }
+
+    handleUserCollections = () => {
+
+    }
+
+    getLoadCollectionInfo = () => {
+
+    }
 
     // componentDidMount() {}
 
-    render(){
-        return(
+    render() {
+        return (
             <div className="Explorer-all">
-                <CanvasOptions />
-                <div className="Explorer-container"> 
-                    <div className="Explorer-canvas"> 
-                        <Canvas 
+                <CanvasOptions 
+                    handleSaveCollection={this.handleSaveCollection}
+                    handleUserCollections={this.handleUserCollections}
+                />
+                <div className="Explorer-container">
+                    <div className="Explorer-canvas">
+                        <Canvas
                             newClass={this.state.newClass}
-                            getNeighbors = {this.getNeighbors}
+                            getNeighbors={this.getNeighbors}
+                            resetCanvas={this.state.resetCanvas}
+                            removeClass={this.state.removeClass}
+                            setCourseObject={this.setCourseObject}
+                            getLoadCollectionInfo={this.getLoadCollectionInfo}
                         />
                     </div>
-                    <div className="Explorer-sideBar"> 
-                        <SideBar handleSearch={this.handleSearch}/>
+                    <div className="Explorer-sideBar">
+                        <SideBar 
+                            handleSearch={this.handleSearch} 
+                            setCourseObject={this.setCourseObject} 
+                            collectionObject={this.state.collectionObject}
+                            handleAddClass={this.handleAddClass}
+                            handleRemoveClass={this.handleRemoveClass}
+                            handleLoadCollection={this.handleLoadCollection}
+                            handleCancel={this.handleCancel} />
                     </div>
                 </div>
             </div>
