e3a5c09e716ce91196cc4f4d9a26d9f76136a061
w-nicole <wnicole1549@gmail.com>
2021-01-16 23:31:39 -0500

    handleUserCOllections

--- client/src/components/modules/CanvasOptions.js
+++ client/src/components/modules/CanvasOptions.js
@@ -20,7 +20,6 @@ class CanvasOptions extends Component {
 
     //componentDidMount(){}
     
-
     render(){
         return(
             
--- client/src/components/modules/UserCollections.js
+++ client/src/components/modules/UserCollections.js
@@ -8,7 +8,7 @@ import "./CanvasOptions.css"
  * Where most of the user inputs are found - searchbar, displaybar, tagbar, etc. 
  *
  * Proptypes
- * @param {paramtype} paramname paramdescription
+ * @param {paramtype} handleUserCollections function from Explorer to switch sidebar state
  *
  */
 
@@ -19,14 +19,14 @@ class UserCollections extends Component {
 
     //componentDidMount(){}
     
-
     render(){
         return(
-            <button className="CanvasOptions-navButton">
+            <button className="CanvasOptions-navButton" onClick={this.props.handleUserCollections}>
                 My Constellations
             </button>
         )
     }
+
 }
 
 export default UserCollections;
