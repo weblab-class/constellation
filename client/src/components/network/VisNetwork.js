d0cf8d5c7f0fc1d5872afd7d4c0a569c369efa50
Ezra Erives <ezraerives@gmail.com>
2021-01-22 20:02:45 -0500

    vis network resizes

--- client/src/components/network/VisNetwork.js
+++ client/src/components/network/VisNetwork.js
@@ -85,6 +85,7 @@ class VisNetwork extends Component {
     this.state={
         prevProcessedClass:'',
         clickToUse: true,
+        autoResize: true,
         options: {
           height: '700px',
           width: '1000px',
@@ -467,6 +468,20 @@ class VisNetwork extends Component {
     if(this.props.canvasToBeReset !== prevProps.canvasToBeReset) this.resetNetwork();
     if(this.props.saveCanvasCounter !== prevProps.saveCanvasCounter) this.saveNetwork();
     if(this.props.loadCollectionCounter !== prevProps.loadCollectionCounter) this.loadNetwork();
+    if(this.props.networkHeight !== prevProps.networkHeight || this.props.networkWidth !== prevProps.networkWidth){
+      this.setState({
+        options: {
+          height: this.props.networkHeight,
+          width: this.props.networkWidth,
+        }
+      })
+      console.log(String(this.props.networkHeight)+'px');
+      console.log(String(this.props.networkWidth)+'px');
+      this.network.setOptions({
+        height: String(this.props.networkHeight)+'px',
+        width: String(this.props.networkWidth)+'px',
+      });
+    } 
   }
 
   render() {
