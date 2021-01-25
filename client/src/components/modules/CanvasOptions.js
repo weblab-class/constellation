897b7f73aa73d8ecc66f107cccc6b9f86388da8f
w-nicole <wnicole1549@gmail.com>
2021-01-25 16:38:10 -0500

    added back to explorer button

--- client/src/components/modules/CanvasOptions.js
+++ client/src/components/modules/CanvasOptions.js
@@ -63,7 +63,6 @@ class CanvasOptions extends Component {
    */
 
 
-
     render() {
         return (
             <>
--- client/src/components/modules/CourseInfo.js
+++ client/src/components/modules/CourseInfo.js
@@ -1,7 +1,5 @@
 import React, { Component } from "react";
 
-import { get } from "../../utilities";
-
 import "./CourseInfo.css";
 
 /**
