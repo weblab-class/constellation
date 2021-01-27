2a7e5e9c48e7d060849712f2f2f62d056880250f
Ezra Erives <ezraerives@gmail.com>
2021-01-27 07:01:00 -0500

    afinalized some wording

--- client/src/components/modules/Menu.js
+++ client/src/components/modules/Menu.js
@@ -26,9 +26,13 @@ class Menu extends Component {
         <div className={classAddOn}>
 
         <div className="Menu-about"><p className="u-bold">What does Constellation do?</p> 
-        <p>Constellation empowers you to dive into the wealth of classes at MIT through a dynamic and interactive experience.</p></div> 
+        <p>Constellation is an interactive course explorer, allowing users to visualize the wealth of classes at MIT, and their interdependencies, in a visually appealing and intuitive manner.
+        </p></div> 
         <div className="Menu-getStarted"><p className="u-bold">Get Started</p> 
-        <p>Intuitively craft, save, and load a directed network of related classes and their requirements, class by class. Get started by logging in!</p>
+        <p> Construct colorful and informative directed networks of classes and their requirements, class by class. Get started by logging in!</p>
+        </div>
+        <div className="Menu-getStarted"><p className="u-bold">About</p> 
+        <p> Constellation was created by Nicole Wong, Ben Wu, and Ezra Erives as part of the 2021 web.lab (web development) competition. This website is made possible by data from the FireRoad API, the vis.js and ReactJS libraries, and, of course, the wonderful teaching staff at web.lab 2021.</p>
         </div>
         </div>
     </>
